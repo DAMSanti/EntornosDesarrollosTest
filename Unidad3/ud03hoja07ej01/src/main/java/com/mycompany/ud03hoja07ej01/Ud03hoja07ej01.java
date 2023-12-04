aee4a8c7aa78066ccdeb02bde935a162375ad639
DAMSanti <santiagomanuel.tamayoarozamena@iesmiguelherrero.com>
2023-12-04 19:57:26 +0100

    correccion ud 03 hoja 07 ej 01

--- Unidad3/ud03hoja07ej01/src/main/java/com/mycompany/ud03hoja07ej01/Ud03hoja07ej01.java
+++ Unidad3/ud03hoja07ej01/src/main/java/com/mycompany/ud03hoja07ej01/Ud03hoja07ej01.java
@@ -7,6 +7,6 @@ package com.mycompany.ud03hoja07ej01;
 public class Ud03hoja07ej01 {
 
     public static void main(String[] args) {
-        System.out.println(Factorial.potencia(5, 5));
+        Factorial.imprimeNumeros(5);
     }
 }
