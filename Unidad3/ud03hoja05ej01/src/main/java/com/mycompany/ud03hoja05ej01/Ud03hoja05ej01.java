1b57c588bed617e07a8ab4b9ba79f3d9e61eaf6e
DAMSanti <santiagomanuel.tamayoarozamena@iesmiguelherrero.com>
2023-12-11 19:21:33 +0100

    comentarios

--- Unidad3/ud03hoja05ej01/src/main/java/com/mycompany/ud03hoja05ej01/Ud03hoja05ej01.java
+++ Unidad3/ud03hoja05ej01/src/main/java/com/mycompany/ud03hoja05ej01/Ud03hoja05ej01.java
@@ -13,15 +13,15 @@ public class Ud03hoja05ej01 {
         Scanner teclado = new Scanner(System.in);
         System.out.println("¿De que altura quieres que sea el triangulo? ");
         int n = teclado.nextInt();
-        for (int i=1; i<=n; i++) {
-            int asteriscos = 1+(i-1)*2;
-            for (int j=1; j<=n-i; j++) {
+        for (int i=1; i<=n; i++) {                  // Un for que representa las filas 
+            int asteriscos = 1+(i-1)*2;             // Expresión que indica el numero de asteriscos en cada fila
+            for (int j=1; j<=n-i; j++) {            // Un for que representa las columnas ocupadas por espacios
                 System.out.print(" ");
             }
-            for (int j=1; j<=asteriscos; j++) {
+            for (int j=1; j<=asteriscos; j++) {     // Un for que representa las columnas ocupadas por asteriscos.
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println("");               // Un println para saltar de linea.
         }
     }
 }
