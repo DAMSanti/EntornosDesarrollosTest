857d50ece052aee910b825648612a3e08ae09771
DAMSanti <santiagomanuel.tamayoarozamena@iesmiguelherrero.com>
2023-12-11 20:06:06 +0100

    .

--- Unidad4/ud04hoja01ej01/src/main/java/unidad4/ud04hoja01ej01/Ud04hoja01ej01.java
+++ Unidad4/ud04hoja01ej01/src/main/java/unidad4/ud04hoja01ej01/Ud04hoja01ej01.java
@@ -9,19 +9,22 @@ public class Ud04hoja01ej01 {
     public static void main(String[] args) {
         int[] numeros = {-2, -2, -2, -2, -2, -2, -2, -2, -2, -2};
         for (int i=0; i<=numeros.length-1; i++) {
-            System.out.println(numeros[i]);
+            System.out.print(numeros[i]);
         }        
         for (int i=0; i<=numeros.length-1; i++) {
             numeros[i] = -1;
         }
+        System.out.println();
         for (int i=0; i<=numeros.length-1; i++) {
-            System.out.println(numeros[i]);
+            System.out.print(numeros[i]);
         }   
+        System.out.println();
         for (int i=0; i<=numeros.length-1; i++) {
             numeros[i] = (int) Math.floor(Math.random()*10);
         }
         for (int i=0; i<=numeros.length-1; i++) {
-            System.out.println(numeros[i]);
+            System.out.print(numeros[i]);
         }   
+        System.out.println();
     }
 }
