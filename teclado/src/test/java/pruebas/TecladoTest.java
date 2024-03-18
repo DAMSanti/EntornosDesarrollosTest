f1a8b581e2ac2939309e66328578d7160bd0ff73
DAMSanti <santiagomanuel.tamayoarozamena@iesmiguelherrero.com>
2024-03-18 15:47:05 +0100

    ..

--- teclado/src/test/java/pruebas/TecladoTest.java
+++ teclado/src/test/java/pruebas/TecladoTest.java
@@ -1,20 +1,10 @@
 package pruebas;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class TecladoTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
 }
