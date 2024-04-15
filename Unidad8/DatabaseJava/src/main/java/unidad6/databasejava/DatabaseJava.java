a9bbfc80725e9b6fe71fa2781948e031a69608bb
DAMSanti <santiagomanuel.tamayoarozamena@iesmiguelherrero.com>
2024-04-15 20:12:49 +0200

    ..

---
+++ Unidad8/DatabaseJava/src/main/java/unidad6/databasejava/DatabaseJava.java
@@ -0,0 +1,12 @@
+package unidad6.databasejava;
+
+/**
+ *
+ * @author DAM104
+ */
+public class DatabaseJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
